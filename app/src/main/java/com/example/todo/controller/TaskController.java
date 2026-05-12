6786d34538a8845807c162720cef9b16f25cd4ac
Altemirov Rahman <krystoll@mail.ru>
2026-05-12 10:25:14 +0400

    ДЕМО: чиним линтер, ломаем coverage (оставили один тест)

--- app/src/main/java/com/example/todo/controller/TaskController.java
+++ app/src/main/java/com/example/todo/controller/TaskController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
-import java.util.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
